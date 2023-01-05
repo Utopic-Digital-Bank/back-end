6ec28d331357ec810a0d460fa83d81d92243914d
Everton Vieira <vieiraeverton33@hotmail.com>
2023-01-05 15:27:45 -0300

    fix: Corrigido urls

--- invoices/urls.py
+++ invoices/urls.py
@@ -4,6 +4,5 @@ from . import views
 from invoices import views as invoices_views
 
 urlpatterns = [
-    path("invoices/", views.InvoiceView.as_view()),
-    path("invoices/<int:pk>/", invoices_views.InvoiceView.as_view()),
+    path("invoices/<int:pk>/", invoices_views.InvoiceDetailView.as_view()),
 ]
