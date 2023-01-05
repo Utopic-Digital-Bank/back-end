ccee6b7ea7f417ee2600b19bb6c1b871b4b17227
wendelbueres <wendelbueres@gmail.com>
2023-01-05 10:30:52 -0300

    feat: :sparkles: serializers investmentCdi

---
+++ InvestmentCdi/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import InvestmentCdi
+
+
+class InvestmentCdiSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = InvestmentCdi
+
+        fields = [
+            "id",
+            "initial_value",
+            "current_value",
+            "yield_value",
+            "creation_date",
+            "account"
+        ]
+
+    def create(self, validated_data):
+        investmentCdi = InvestmentCdi.objects.create(**validated_data)
+
+        return investmentCdi
