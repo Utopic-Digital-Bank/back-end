bbb37b649c90b73f367d151d79ee0eaf8afe0a50
wendelbueres <wendelbueres@gmail.com>
2023-01-06 17:13:21 -0300

    feat: :sparkles: criada view economicConsultant

--- economicConsultant/views.py
+++ economicConsultant/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .serializers import EconomicConsultantSerializer
+
+
+class EconomicConsultantView(CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
+    serializer_class = EconomicConsultantSerializer
