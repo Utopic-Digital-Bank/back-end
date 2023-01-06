c1533a492ed59e0301e30c9abba6e189aeb7591d
wendelbueres <wendelbueres@gmail.com>
2023-01-06 15:20:39 -0300

    feat: :sparkles: adicionado create

--- InvestmentCdi/serializers.py
+++ InvestmentCdi/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from .models import InvestmentCdi
+from datetime import date
+import requests
+import json
+import ipdb
 
 
 class InvestmentCdiSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
+    creation_date = serializers.DateField(required=False)
 
     class Meta:
         model = InvestmentCdi
@@ -17,7 +21,23 @@ class InvestmentCdiSerializer(serializers.ModelSerializer):
             "account"
         ]
 
+        read_only_fields = ['id', 'current_value',
+                            'yield_value', 'creation_date', 'account',]
+
     def create(self, validated_data):
+        data_fim = date.today()
+        data_fim = data_fim.strftime("%Y-%m-%d")
+        data_inicio = data_fim
+        value = validated_data.get("initial_value")
+        value = float(value)
+        url = f"https://calculadorarendafixa.com.br/calculadora/di/calculo?dataInicio={data_inicio}&dataFim={data_fim}&percentual=100.00&valor={value}"
+
+        response_b3 = requests.get(url)
+        response_b3 = json.loads(response_b3.content)
+        validated_data["current_value"] = float(response_b3["valorCalculado"])
+        validated_data["yield_value"] = (
+            float(response_b3["valorCalculado"]) - float(response_b3["valorBase"]))
+
         investmentCdi = InvestmentCdi.objects.create(**validated_data)
 
         return investmentCdi
