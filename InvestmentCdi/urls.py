ac3013a2b5057d41d755a6e8c29adc728e502726
wendelbueres <wendelbueres@gmail.com>
2023-01-06 23:57:54 -0300

    fix: :bug: corrigida chamada de view

--- InvestmentCdi/urls.py
+++ InvestmentCdi/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("investment-cdi/", views.InvestmentCdiView.as_view()),
+    path("investment-cdi/", views.ListCreateInvestmentCdiView.as_view()),
     path("investment-cdi/<int:pk>/",
          views.InvestmentCdiDetailView.as_view()),
 ]
