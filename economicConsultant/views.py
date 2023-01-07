f3199d4370e6c6582a75c026b94347fff2bc86f8
wendelbueres <wendelbueres@gmail.com>
2023-01-06 21:58:30 -0300

    feat: :sparkles: adicionado queryset

--- economicConsultant/views.py
+++ economicConsultant/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from .models import EconomicConsultant
 from .serializers import EconomicConsultantSerializer
 
 
@@ -10,3 +11,4 @@ class EconomicConsultantView(CreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAdminUser]
     serializer_class = EconomicConsultantSerializer
+    queryset = EconomicConsultant.objects.all()
