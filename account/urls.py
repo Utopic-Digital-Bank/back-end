e4097d8736228f4990e79f2c2166ce2fcdff37c7
wendelbueres <wendelbueres@gmail.com>
2023-01-06 22:06:12 -0300

    fix: :bug: consertada rotas de account

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("account/", views.AccountView.as_view()),
     path("account/<int:pk>/", views.AccountView.as_view()),
-    path("account/<int:account_id>/", include("InvestmentCdi.urls"),
+    path("account/<int:account_id>/", include("InvestmentCdi.urls")),
     # path("account/insurance/", #Views de insurance#,
     # path("account/economic_consultant/ #Views de account#")#
-
+]
