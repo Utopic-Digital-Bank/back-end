51edf759d6e6bab237f8402b2c058d7d05da54d8
Everton Vieira <vieiraeverton33@hotmail.com>
2023-01-09 16:56:01 -0300

    Fix: Refatoração da lógica do serializer

--- invoices/serializers.py
+++ invoices/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
+from launch.serializers import LaunchSerializer
 from .models import Invoice
+from datetime import date, datetime
 import ipdb
-from datetime import date, timedelta, datetime
-from dateutil.relativedelta import relativedelta
 
 class InvoiceSerializer(serializers.ModelSerializer):
+    launch = LaunchSerializer(many=True, read_only=True)
+    #value = serializers.SerializerMethodField(method_name='sub_total')
 
     class Meta:
         model = Invoice
@@ -15,39 +17,38 @@ class InvoiceSerializer(serializers.ModelSerializer):
             'closing_date',
             'paid',
             'due_date',
+            'launch',
         ]
         read_only_fields = [
-            'id', 
-            'closing_date', 
-            'due_date', 
-            'month_reference', 
             'value',
+            'due_date', 
+            'month_reference',
+            'launch',
         ]
     
     def create(self, validated_data):
         card = validated_data['card']
-        objeto = Invoice()
-        data = timedelta(days=int(card.due_date))
-        due_days = int(card.due_date)
-        objeto.value = 400.00
-        objeto.card_id = 1
+        object = Invoice()
+        object.card_id = card.id
+
         current_date = date.today()
         current_day = current_date.strftime('%d')
         current_month = current_date.strftime('%m')
         current_year = current_date.strftime('%Y')
-        
+
         if int(current_day) >= int(card.due_date):
             if int(current_month) == 12:
                 current_year = int(current_year) + 1
                 current_month = 1
             current_month = int(current_month) + 1
         date_reference = datetime(int(current_year), int(current_month), int(card.due_date))
-        closing_date = datetime(int(current_year), int(current_month), (int(card.due_date)-5))
-        objeto.closing_date = closing_date.strftime("%Y-%m-%d")
-        objeto.month_reference = str(date_reference.strftime("%Y-%m-%d"))
-        objeto.due_date = str(date_reference.strftime("%Y-%m-%d"))
-        objeto.save()
-        return objeto
+        closing_date = validated_data.get('closing_date')
+        object.closing_date = closing_date
+        object.month_reference = str(date_reference.strftime("%Y-%m-%d"))
+        object.due_date = str(date_reference.strftime("%Y-%m-%d"))
+        object.save()
+        return object
+
 
     def update(self, instance: Invoice, validated_data: dict) -> Invoice:        
         for key, value in validated_data.items():            
@@ -58,3 +59,6 @@ class InvoiceSerializer(serializers.ModelSerializer):
                     raise ValueError(f"It's not possible to change the payment status")
             else:
                 raise KeyError(f"The parameter {key} not is alterabled")
+
+    def sub_total(self, obj:Invoice):
+        return int(200)
