24153d24e27f0f024b0cf546242729a78d8174d1
wendelbueres <wendelbueres@gmail.com>
2023-01-09 17:39:01 -0300

    fix: :bug: field creation_date

--- extract/models.py
+++ extract/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
+
 class OperationOptions(models.TextChoices):
     PIX = "pix",
-    TRANSFERÊNCIA = "transferência", 
+    TRANSFERÊNCIA = "transferência",
     SAQUE = "saque",
     PAGAMENTO = "pagamento"
     DEPÓSITO = "depósito"
 
+
 class Extract(models.Model):
-    
+
     # Detalhes do faturamento(Valor anterior e Valor atual)
-    valueOperation = models.FloatField(max_length=17, default= 00.1)
+    valueOperation = models.FloatField(max_length=17, default=00.1)
     previous_balance = models.FloatField(max_length=17)
     current_balance = models.FloatField(max_length=17)
 
     # Referencias do Faturamento(Tipo de Operação, Data Criação e Chave Estrangeira da 'conta')
-    operation = models.CharField(max_length=13, choices=OperationOptions.choices)
-    created_at = models.DateTimeField()
-    account = models.ForeignKey("account.Account", related_name="extract", on_delete=models.PROTECT)
+    operation = models.CharField(
+        max_length=13, choices=OperationOptions.choices)
+    creation_date = models.DateTimeField()
+    account = models.ForeignKey(
+        "account.Account", related_name="extract", on_delete=models.PROTECT)
