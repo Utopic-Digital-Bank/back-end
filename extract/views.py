7fb430daa479511caf5b86ad465d67d3d5601784
wendelbueres <wendelbueres@gmail.com>
2023-01-09 11:03:57 -0300

    fix: importações

--- extract/views.py
+++ extract/views.py
@@ -1,5 +1,4 @@
-from .models import Account
-from .serializers import AccountSerializer, UpdateBalance
+from account.serializers import AccountSerializer
 from extract.serializers import ExtractSerializer
 from rest_framework.pagination import PageNumberPagination
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -13,33 +12,34 @@ from insurance.models import Insurance
 from .models import Extract
 from account.models import Account
 
+
 class ListExtract (generics.ListAPIView):
-        authentication_classes = [JWTAuthentication]
-        permission_classes = [IsAuthenticated, IsAccountOwner]
-        serializer_class = Extract
-        queryset = Extract.objects.all()
-       
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAccountOwner]
+    serializer_class = Extract
+    queryset = Extract.objects.all()
+
 
 class CreateExtract(generics.ListCreateAPIView):
-        authentication_classes = [JWTAuthentication]
-        permission_classes = [IsAuthenticated, IsAccountOwner]
-        serializer_class = Extract
-        queryset = Extract.objects.all()
-        lookup_url_kwarg = "pk"
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAccountOwner]
+    serializer_class = Extract
+    queryset = Extract.objects.all()
+    lookup_url_kwarg = "pk"
 
-        def perform_create(self, serializer):
- #   Atualiza somente o balance
-            account = get_object_or_404(Account, id = self.kwargs["pk"])
-            valueOperation = self.valueOperation
-            self.previous_balance = account.balance
-            
-            if self.operation == "DEPOSITO":
-                self.current_balance = self.valueOperation + self.previous_balance
-                account.balance = (self.previous_balance + valueOperation)
-                account.save()
-            else:
-                self.current_balance = self.valueOperation - self.previous_balance
-                self.previous_balance = self.current_balance
-                account.balance = (self.previous_balance - valueOperation)
-                account.save()
-            serializer.save(self)
+    def perform_create(self, serializer):
+     #   Atualiza somente o balance
+        account = get_object_or_404(Account, id=self.kwargs["pk"])
+        valueOperation = self.valueOperation
+        self.previous_balance = account.balance
+
+        if self.operation == "DEPOSITO":
+            self.current_balance = self.valueOperation + self.previous_balance
+            account.balance = (self.previous_balance + valueOperation)
+            account.save()
+        else:
+            self.current_balance = self.valueOperation - self.previous_balance
+            self.previous_balance = self.current_balance
+            account.balance = (self.previous_balance - valueOperation)
+            account.save()
+        serializer.save(self)
