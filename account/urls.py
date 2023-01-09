f9068d695e85b084603229c27fc8c61565730e61
wendelbueres <wendelbueres@gmail.com>
2023-01-09 17:37:31 -0300

    refactor: :hammer: ajustados endpoints

--- account/urls.py
+++ account/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("account/<int:pk>/", views.AccountDetails.as_view()),
     path("account/<int:account_id>/", include("InvestmentCdi.urls")),
     path("card/<int:pk>/", CardViews.CardView.as_view()),
-    path("account/<int:account_id>/operation", ExtractViews.CreateExtract.as_view()),
-    path("account/<int:account_id>/extract", ExtractViews.ListExtract.as_view()),
+    path("account/<int:account_id>/operation/",
+         ExtractViews.CreateExtract.as_view()),
+    path("account/<int:account_id>/extract/",
+         ExtractViews.ListExtract.as_view()),
 ]
