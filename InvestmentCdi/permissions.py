66f1f9c76c12b57a7f1eb1e9fad435fea781bc15
wendelbueres <wendelbueres@gmail.com>
2023-01-09 21:06:39 -0300

    fix: :bug: permissions

--- InvestmentCdi/permissions.py
+++ InvestmentCdi/permissions.py
@@ -1,15 +1,20 @@
 from rest_framework import permissions
 from users.models import User
+from account.models import Account
 from rest_framework.views import View
+import ipdb
 
 
 class IsAccountOwner(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: User) -> bool:
-        return obj == request.user
-
-
-class IsAdmOrAccountOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view: View, obj: User) -> bool:
-        if not request.user.is_superuser:
-            return obj == request.user
-        return True
+        account = Account.objects.get(id=obj.account_id)
+        if (request.method == "POST" or request.method == "DELETE"):
+            if (account.user == request.user):
+                return True
+            else:
+                return False
+        if (account.user == request.user):
+            return True
+        if request.user.is_superuser:
+            return True
+        return False
