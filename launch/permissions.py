8bb1c52ea7b13dae41eeee1ba86145856f1f8500
wendelbueres <wendelbueres@gmail.com>
2023-01-10 16:31:59 -0300

    fix: :bug: permissions launch

--- launch/permissions.py
+++ launch/permissions.py
@@ -1,8 +1,15 @@
 from rest_framework import permissions
 from users.models import User
+from account.models import Account
+from .models import Launch
 from rest_framework.views import View
+import ipdb
 
 
 class IsAccountOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view: View, obj: User) -> bool:
-        return obj == request.user
+
+    def has_permission(self, request, view):
+        account = Account.objects.get(
+            id=request.stream.resolver_match.kwargs["account_id"])
+
+        return account.user.id == request.user.id
