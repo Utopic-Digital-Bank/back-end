9cec490b89ff15286cecf66603bbca09a1eff902
Everton Vieira <vieiraeverton33@hotmail.com>
2023-01-11 19:27:32 -0300

    Fix: Corrigido lógica da invoice

--- invoices/serializers.py
+++ invoices/serializers.py
@@ -3,6 +3,10 @@ from launch.serializers import LaunchSerializer
 from .models import Invoice
 from datetime import date, datetime
 import ipdb
+from django.shortcuts import get_object_or_404
+from account.models import Account
+from card.models import Card
+
 
 
 class InvoiceSerializer(serializers.ModelSerializer):
@@ -18,11 +22,13 @@ class InvoiceSerializer(serializers.ModelSerializer):
             'paid',
             'due_date',
             'launch',
+            'card',
         ]
         read_only_fields = [
             'value',
             'due_date',
             'launch',
+            'card',
         ]
 
     def create(self, validated_data):
@@ -52,13 +58,28 @@ class InvoiceSerializer(serializers.ModelSerializer):
     def update(self, instance: Invoice, validated_data: dict) -> Invoice:
         for key, value in validated_data.items():
             if key == 'paid':
-                if (value is True):
+                if value == True:
                     setattr(instance, key, value)
                 else:
                     raise ValueError(
                         f"It's not possible to change the payment status")
             else:
                 raise KeyError(f"The parameter {key} not is alterabled")
+            #ipdb.set_trace()
+            card = get_object_or_404(Card, id=self.data['card'])
+            account = get_object_or_404(Account, id=card.account_id)
+            #ipdb.set_trace()
+            #if instance.paid == True:
+                #raise serializers.ValidationError({"validationError": "It's not possible to change the payment status"})
+            #ipdb.set_trace()
+            if account.balance >= self.data['value']:
+                account.balance -= self.data['value']
+                instance.paid = True
+                instance.save()
+                account.save()
+            else:
+                raise serializers.ValidationError({"balanceError": "Balance of account insufficient"})
+        return instance
 
     def sub_total(self, obj: Invoice):
-        return int(200)
+        return sum(obj.value)
