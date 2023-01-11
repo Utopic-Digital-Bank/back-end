298e293b4834fc13b829073cd5d44123365b5974
wendelbueres <wendelbueres@gmail.com>
2023-01-10 23:56:33 -0300

    refactor: :hammer: rota de cartão alterada

--- launch/urls.py
+++ launch/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("account/<account_id>/card/<card_id>/launch/",
+    path("card/<card_id>/launch/",
          views.LaunchView.as_view()),
 ]
