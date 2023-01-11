82ad046481274695fe5e2118f75e20bc5b0996db
Caio S.G <caiosilvestre.dev@gmail.com>
2023-01-11 14:35:58 -0300

    permissions Account

--- account/permissions.py
+++ account/permissions.py
@@ -2,13 +2,28 @@ from rest_framework import permissions
 from users.models import User
 from .models import Account
 from rest_framework.views import View
+import ipdb
 
 
 class IsAccountOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view: View, obj:Account) -> bool:
-        if request.method == "PATCH":
-            return request.user.is_superuser
-        if request.user.is_superuser:
+    def has_permission(self, request, view) -> bool:
+        account_id = request.parser_context["kwargs"]["pk"]
+        account = Account.objects.filter(id=account_id)
+        if not account:
             return True
-        return obj.user_id == request.user.id
+        account = Account.objects.get(id=account_id)
 
+        return (account.user_id == request.user.id)
+
+
+class IsUserOrAdmin(permissions.BasePermission):
+    def has_permission(self, request, view) -> bool:
+        if request.method == "POST":
+            return request.user.is_authenticated
+        account_id = request.parser_context["kwargs"]["account_id"]
+        account = Account.objects.filter(id=account_id)
+        if not account:
+            return True
+        account = Account.objects.get(id=account_id)
+
+        return (account.user_id == request.user.id) or (request.user.is_superuser)
