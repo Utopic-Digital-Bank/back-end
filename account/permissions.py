1966ed1dd04fe5481bb7a080bb6253675f6c8a48
wendelbueres <wendelbueres@gmail.com>
2023-01-10 21:09:30 -0300

    fix: :bug: permissions

--- account/permissions.py
+++ account/permissions.py
@@ -2,13 +2,24 @@ from rest_framework import permissions
 from users.models import User
 from .models import Account
 from rest_framework.views import View
+import ipdb
 
 
 class IsAccountOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view: View, obj:Account) -> bool:
-        if request.method == "PATCH":
-            return request.user.is_superuser
-        if request.user.is_superuser:
-            return True
-        return obj.user_id == request.user.id
 
+    def has_permission(self, request, view) -> bool:
+        account = Account.objects.filter(
+            user_id=request.parser_context["kwargs"]["pk"])
+        if not account:
+            return True
+
+        return (account.user_id == request.user.id) or (request.user.is_superuser)
+
+
+class IsUserOrAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+
+        if request.method == "GET":
+            return request.user.is_superuser
+
+        return super().has_permission(request, view)
