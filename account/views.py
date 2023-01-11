239be7239e0bbea88ae00723672701a1f297fbe6
wendelbueres <wendelbueres@gmail.com>
2023-01-10 21:12:29 -0300

    feat: :sparkles: erro ao tentar criar conta duplicada

--- account/views.py
+++ account/views.py
@@ -1,17 +1,11 @@
 from .models import Account
 from .serializers import AccountSerializer, UpdateAccount
-from extract.serializers import ExtractSerializer
 from rest_framework.pagination import PageNumberPagination
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .permissions import IsAccountOwner
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .permissions import IsAccountOwner, IsUserOrAdmin
 from rest_framework import generics
-from django.shortcuts import get_object_or_404
-from economicConsultant.models import EconomicConsultant
-from insurance.models import Insurance
-from extract.models import Extract
 from drf_spectacular.utils import extend_schema
+from rest_framework.views import Response, status
 
 import ipdb
 
@@ -21,13 +15,19 @@ from drf_spectacular.utils import extend_schema
 @extend_schema(tags=["account"])
 class AccountView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAdminUser, IsAuthenticated]
+    permission_classes = [IsUserOrAdmin]
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
     pagination_class = PageNumberPagination
 
-    def perform_create(self, serializer):
+    def create(self, request, *args, **kwargs):
+        account = Account.objects.filter(user_id=request.user.id)
+        if account:
+            return Response(
+                {"hasAccount": "It is not allowed to have more than one account."}, status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
 
+    def perform_create(self, serializer):
         serializer.save(user_id=self.request.user.id)
 
 
@@ -35,10 +35,12 @@ class AccountView(generics.ListCreateAPIView):
 @extend_schema(methods=["PUT"], exclude=True)
 class AccountDetails(generics.RetrieveUpdateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated, IsAccountOwner]
+    permission_classes = [IsAccountOwner]
     serializer_class = UpdateAccount
-    queryset = Account.objects.all()
     lookup_url_kwarg = "pk"
+
+    def get_queryset(self):
+        return Account.objects.get(id=self.kwargs['pk'])
 
     # def perform_update(self, serializer):
     #     insuranceGet = get_object_or_404(Insurance, name = self.request.name)
