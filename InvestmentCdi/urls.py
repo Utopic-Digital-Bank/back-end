894557b0681de81c0ce64a7ec33deb6fb300178f
wendelbueres <wendelbueres@gmail.com>
2023-01-12 02:18:28 -0300

    fix: :bug: urls

--- InvestmentCdi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from . import views
-
-urlpatterns = [
-    path("investment-cdi/", views.ListCreateInvestmentCdiView.as_view()),
-    path("investment-cdi/<int:pk>/",
-         views.InvestmentCdiDetailView.as_view()),
-]
--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path, include
 from . import views
 
-from insurance import views as InsuranceViews
+from InvestmentCdi import views as CdiViews
 from card import views as CardViews
 from extract import views as ExtractViews
 
 urlpatterns = [
     path("account/", views.AccountView.as_view()),
     path("account/<int:pk>/", views.AccountDetails.as_view()),
-    path("account/<int:account_id>/", include("InvestmentCdi.urls")),
+    path("account/<int:account_id>/investment-cdi/",
+         CdiViews.ListCreateInvestmentCdiView.as_view()),
+    path("account/<int:account_id>/investment-cdi/<int:pk>/",
+         CdiViews.InvestmentCdiDetailView.as_view()),
     path("account/card/", CardViews.CardView.as_view()),
     path("account/card/<int:card_id>/",
          CardViews.CardDetailView.as_view()),
